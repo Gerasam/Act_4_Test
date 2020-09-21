36258561974b4544140d5d4bccbfe185f12dc02e
Gerardo Vazquez <gerardo.vazquez9084@alumnos.udg.mx>
2020-09-21 17:04:26 -0500

    Funcionan las opciones 3 y 4

--- Act.4.c
+++ Act.4.c
@@ -1,5 +1,42 @@
 #include<stdio.h>
 
+struct personaje
+{
+    char nombre[20];
+    char tipo[20];
+    float fuerza;
+    int salud;
+}personajes[5];
+
+unsigned int cont=0;
+
+void capturar_personajes(){
+    if(cont<5){
+    printf("Nombre: ");
+    scanf("%s",personajes[cont].nombre);
+    printf("Tipo: ");
+    scanf("%s",personajes[cont].tipo);
+    printf("Fuerza: ");
+    scanf("%f",&personajes[cont].fuerza);
+    printf("Salud: ");
+    scanf("%i",&personajes[cont].salud);
+    cont++;
+    }
+    else{
+        printf("El arreglo de personajes esta lleno");
+    }
+}
+
+void mostrar_personajes(){
+    for (size_t i = 0; i < cont; i++)
+    {
+        printf("\n\nNombre: %s",personajes[i].nombre);
+        printf("\nTipo: %s",personajes[i].tipo);
+        printf("\nFuerza: %.2f",personajes[i].fuerza);
+        printf("\nSalud: %i",personajes[i].salud);
+    }
+    
+}
 
 void capturar_enteros(){
     int enteros[5];
@@ -57,8 +94,10 @@ int main(){
                 mostrar(n,cadena);
                 break;
             case 3:
+                capturar_personajes();
                 break;
             case 4:
+                mostrar_personajes();
                 break;
         }
     } while (m!=0);
